d91278c2abf900bde7d87781e12388a4abf3db99
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2026-04-19 14:40:33 +0530

    FSR CHECK

---
+++ FSR_CIRUIT.ino
@@ -0,0 +1,13 @@
+const int FSR_PIN = 34;
+
+void steup(){
+    Serial.begin(115200);
+
+}
+
+void loop(){
+    int fsrValue = analogRead(FSR_PIN);
+    Serial.print("FSR Value: ");
+    Serial.println(fsrValue);
+    delay(200); // Delay for 1 second before the next reading
+}
